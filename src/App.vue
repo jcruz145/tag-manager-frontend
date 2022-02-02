3c22dbcb1bd6d2cdfad73ac2c8b6e62613113c60
Jan Cruz <jcruz145@gmail.com>
2022-02-02 03:22:45 -0500

    started stepper integration with welcome, categories, and historicals

--- src/App.vue
+++ src/App.vue
@@ -9,9 +9,49 @@
       </v-btn>
     </v-app-bar>
     <v-main>
+      <v-container>
+        <v-stepper vertical v-model="stepperCount">
+          <v-stepper-step
+            step="1"
+            @click="stepperCount = 1"
+            :complete="stepperData.welcomeSeen"
+          >
+            Welcome to TagManager!
+          </v-stepper-step>
+          <v-stepper-content step="1">
+            This application helps you optimize your social media posts by
+            helping you choose the right hashtags to drive meaningful and
+            efficient discovery of your content.
+          </v-stepper-content>
+          <v-stepper-step
+            step="2"
+            @click="stepperCount = 2"
+            :complete="atLeastOneCategorySelected"
+            :rules="[() => !categoriesError]"
+          >
+            Select Categories
+            <small v-if="categoriesError"
+              >You must select atleast 1 category</small
+            >
+          </v-stepper-step>
+          <v-stepper-content step="2">
+            <category-selector></category-selector>
+          </v-stepper-content>
+          <v-stepper-step
+            step="3"
+            @click="stepperCount = 3"
+            :complete="completeHistories.length > 0"
+          >
+            Historical Tags
+          </v-stepper-step>
+          <v-stepper-content step="3">
+            <historical-tags></historical-tags>
+          </v-stepper-content>
+        </v-stepper>
+      </v-container>
+    </v-main>
+    <v-main>
       <v-expansion-panels>
-        <category-selector></category-selector>
-        <historical-tags></historical-tags>
         <quantity-selector></quantity-selector>
         <tag-generator></tag-generator>
       </v-expansion-panels>
@@ -24,10 +64,34 @@ import CategorySelector from "./components/category-selector/CategorySelector.vu
 import HistoricalTags from "./components/historical-tags/HistoricalTags.vue";
 import QuantitySelector from "./components/quantity-selector/QuantitySelector.vue";
 import TagGenerator from "./components/tag-generator/TagGenerator.vue";
-import { mapActions } from "vuex";
+import { mapActions, mapGetters } from "vuex";
 
 export default {
   name: "App",
+
+  computed: {
+    ...mapGetters([
+      "stepperData",
+      "selectedCategories",
+      "completeHistories",
+      "atLeastOneCategorySelected",
+    ]),
+    stepperCount: {
+      set(value) {
+        this.setStepperTracker({ tracker: value });
+      },
+      get() {
+        return this.stepperData.tracker;
+      },
+    },
+    categoriesError() {
+      return (
+        this.stepperData.categoriesSeen &&
+        !this.atLeastOneCategorySelected &&
+        this.stepperData.tracker != 2
+      );
+    },
+  },
 
   components: {
     CategorySelector,
@@ -37,7 +101,7 @@ export default {
   },
 
   methods: {
-    ...mapActions(["clearAppData"]),
+    ...mapActions(["clearAppData", "setStepperTracker"]),
   },
 };
 </script>
--- src/components/historical-tags/HistoricalTags.vue
+++ src/components/historical-tags/HistoricalTags.vue
@@ -1,42 +1,27 @@
 <template>
-  <v-container>
-    <v-expansion-panel>
-      <v-expansion-panel-header>
-        <template>
-          <v-row no-gutters>
-            <v-col cols="12"> Historical Tags </v-col>
-            <v-col cols="12">
-              <v-container
-                ><v-row>
-                  <v-col
-                    class="shrink"
-                    v-for="history in histories"
-                    :key="history.id"
-                  >
-                    <v-chip
-                      :color="history.value ? 'green' : ''"
-                      :text-color="history.value ? 'white' : ''"
-                      :input-value="history.value"
-                      filter
-                    >
-                      {{ history.label }}
-                    </v-chip>
-                  </v-col>
-                </v-row></v-container
-              >
-            </v-col>
-          </v-row>
-        </template>
-      </v-expansion-panel-header>
-      <v-expansion-panel-content>
-        <history-capture
-          v-for="history in histories"
-          :key="history.id"
-          :histData="history"
-        ></history-capture>
-      </v-expansion-panel-content>
-    </v-expansion-panel>
-  </v-container>
+  <v-row no-gutters>
+    <v-col cols="12">
+      <v-chip
+        v-for="history in histories"
+        :key="history.id"
+        :color="history.value ? 'green' : ''"
+        :text-color="history.value ? 'white' : ''"
+        :input-value="history.value"
+        filter
+        class="ml-1 my-1"
+        small
+      >
+        {{ history.label }}
+      </v-chip>
+    </v-col>
+    <v-col>
+      <history-capture
+        v-for="history in histories"
+        :key="history.id"
+        :histData="history"
+      ></history-capture>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
